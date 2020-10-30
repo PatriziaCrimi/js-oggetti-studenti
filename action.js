96ec37216d451d7a4060f24826515227e88d6987
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-30 15:48:10 +0100

    Basic HTML & CSS structure set

---
+++ action.js
@@ -0,0 +1,16 @@
+/* ASSIGNMENT
+1. Creare un oggetto che rappresenta uno studente:
+ogni studente è caratterizzato da un nome, un cognome e un'età.
+Attraverso un ciclo for-in stampare a schermo tutte le sue proprietà e il relativo valore
+2. Creare un array di oggetti "studente".
+Ciclare su tutti gli studenti e stampare per ciascuno il nome e il cognome
+3. Tramite 3 prompt, chiedere all'utente il nome, il cognome e l'età di un nuovo studente.
+Creare con questi dati un nuovo oggetto
+e inserirlo nell'array del punto precedente.
+*/
+
+// Variables and constants initialization
+
+$(document).ready(function () {
+  alert('test');
+});
